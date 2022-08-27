affd4cce64583d11b0da0e350f7126e41893ac97
Justin Hold <36420615+jhold1982@users.noreply.github.com>
2022-08-27 13:30:31 -0500

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Justin Hold on 8/27/22.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
