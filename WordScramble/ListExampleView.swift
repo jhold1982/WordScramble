c4543599962b8a3f31fb2a4665e8eba16365bcff
Justin Hold <36420615+jhold1982@users.noreply.github.com>
2023-02-24 12:23:17 -0600

    first commit

---
+++ WordScramble/ListExampleView.swift
@@ -0,0 +1,20 @@
+//
+//  ListExampleView.swift
+//  WordScramble
+//
+//  Created by Justin Hold on 2/24/23.
+//
+
+import SwiftUI
+
+struct ListExampleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ListExampleView_Previews: PreviewProvider {
+    static var previews: some View {
+        ListExampleView()
+    }
+}
---
+++ WordScramble/StringsExampleView.swift
@@ -0,0 +1,20 @@
+//
+//  StringsExampleView.swift
+//  WordScramble
+//
+//  Created by Justin Hold on 2/24/23.
+//
+
+import SwiftUI
+
+struct StringsExampleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct StringsExampleView_Previews: PreviewProvider {
+    static var previews: some View {
+        StringsExampleView()
+    }
+}
