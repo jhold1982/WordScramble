a26ab39c166a91655f7e793cb29f9f4d9acbedc6
Justin Hold <36420615+jhold1982@users.noreply.github.com>
2023-02-24 12:29:52 -0600

    commented out example views

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -5,21 +5,30 @@
 //  Created by Justin Hold on 8/27/22.
 //
 import SwiftUI
+
 struct ContentView: View {
+	
+	@State private var usedWords = [String]()
+	@State private var rootWord = ""
+	@State private var newWord = ""
+	
     @State private var score = 0
-    @State private var usedWords = [String]()
-    @State private var rootWord = ""
-    @State private var newWord = ""
+    
     @State private var errorTitle = ""
     @State private var errorMessage = ""
     @State private var showingError = false
+	
     var body: some View {
+		
         NavigationStack {
+			
             List {
+				
                 Section {
                     TextField("Enter your word", text: $newWord)
                         .autocapitalization(.none)
                 }
+				
                 Section {
                     ForEach(usedWords, id: \.self) { word in
                         HStack {
@@ -53,6 +62,7 @@ struct ContentView: View {
             }
         }
     }
+	
     func addNewWord() {
         let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
         guard answer.count > 3 else {
@@ -81,6 +91,7 @@ struct ContentView: View {
         newWord = ""
         score += answer.count
     }
+	
     func startGame() {
         newWord = ""
         usedWords.removeAll()
@@ -94,9 +105,11 @@ struct ContentView: View {
         }
         fatalError("Could not load start.txt from bundle.")
     }
+	
     func isOriginal(word: String) -> Bool {
         !usedWords.contains(word)
     }
+	
     func isPossible(word: String) -> Bool {
         var tempWord = rootWord
         for letter in word {
@@ -108,18 +121,21 @@ struct ContentView: View {
         }
         return true
     }
+	
     func isReal(word: String) -> Bool {
         let checker = UITextChecker()
         let range = NSRange(location: 0, length: word.utf16.count)
         let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
         return misspelledRange.location == NSNotFound
     }
+	
     func wordError(title: String, message: String) {
         errorTitle = title
         errorMessage = message
         showingError = true
     }
 }
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
--- WordScramble/StringsExampleView.swift
+++ WordScramble/StringsExampleView.swift
@@ -12,37 +12,37 @@ struct StringsExampleView: View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 	
-	func test1() {
-		let input = "a b c"
-		let letters = input.components(separatedBy: " ")
-	}
+//	func test1() {
+//		let input = "a b c"
+//		let letters = input.components(separatedBy: " ")
+//	}
 	
-	func test2() {
-		let input = """
-a
-b
-c
-"""
-		let letters = input.components(separatedBy: "\n")
-		let letter = letters.randomElement()
-		
-		let trimmed = letter?.trimmingCharacters(in: .whitespacesAndNewlines)
-	}
+//	func test2() {
+//		let input = """
+//a
+//b
+//c
+//"""
+//		let letters = input.components(separatedBy: "\n")
+//		let letter = letters.randomElement()
+//
+//		let trimmed = letter?.trimmingCharacters(in: .whitespacesAndNewlines)
+//	}
 	
-	func test3() {
-		let word = "swift"
-		let checker = UITextChecker()
-		
-		let range = NSRange(location: 0, length: word.utf16.count)
-		let misspelledRange = checker.rangeOfMisspelledWord(
-			in: word,
-			range: range,
-			startingAt: 0,
-			wrap: false,
-			language: "en"
-		)
-		let allGood = misspelledRange.location == NSNotFound
-	}
+//	func test3() {
+//		let word = "swift"
+//		let checker = UITextChecker()
+//		
+//		let range = NSRange(location: 0, length: word.utf16.count)
+//		let misspelledRange = checker.rangeOfMisspelledWord(
+//			in: word,
+//			range: range,
+//			startingAt: 0,
+//			wrap: false,
+//			language: "en"
+//		)
+//		let allGood = misspelledRange.location == NSNotFound
+//	}
 }
 
 struct StringsExampleView_Previews: PreviewProvider {
