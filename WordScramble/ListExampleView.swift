f38ef40f101d2dabc9cf8115e0ce12ef424d60ef
Justin Hold <36420615+jhold1982@users.noreply.github.com>
2023-02-24 12:23:29 -0600

    Initial commit

--- WordScramble/ListExampleView.swift
+++ WordScramble/ListExampleView.swift
@@ -7,10 +7,53 @@
 
 import SwiftUI
 
+// List(0..<5) {
+//		Text("Dynamic List Row \(0)")
+//	}
+
+// let people = ["People 1", "People 2", "People 3"]
+
+// List(people, id: \.self)
+
 struct ListExampleView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+		
+		List {
+			
+		
+			Section("Section 1") {
+				
+				Text("Static Row 1")
+				Text("Static Row 2")
+			}
+		
+			Section("Section 2 - Dynamic") {
+				ForEach(0..<5) {
+					Text("Dynamic row \($0)")
+				}
+			}
+			
+			Section("Section 3") {
+				Text("Static Row 4")
+				Text("Static Row 5")
+			}
+		}
+		// makes everything edge-to-edge
+//		.listStyle(.grouped)
     }
+	
+	
+//	func loadFile() {
+//		if let fileURL = Bundle.main.url(
+//			forResource: "some-file",
+//			withExtension: ".txt"
+//		){
+//			if let fileContents = try? String(contentsOf: fileURL) {
+//				fileContents
+//			}
+//		}
+//	}
+	
 }
 
 struct ListExampleView_Previews: PreviewProvider {
